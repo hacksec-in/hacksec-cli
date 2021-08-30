acf6175c6a83328778b4b08cb810f4c371931aeb
burpdroid <script1337x@gmail.com>
2021-08-30 06:00:11 +0530

    add install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install git python3 python3-pip -y
 git clone https://github.com/ScRiPt1337/hacksec-cli ~/hacksec-cli
-pip install -r  ~/hacksec-cli/hacksec_cli/requirements.txt
-sudo echo "python3 ~/hacksec-cli/hacksec_cli/app.py" > /usr/local/bin/hacksec
+pip install -r  ~/hacksec-cli/requirements.txt
+sudo echo "python3 ~/hacksec-cli/hacksec_cli/app.py" > /usr/bin/hacksec
