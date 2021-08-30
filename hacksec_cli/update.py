95303aec6807fd7492141af84ea6ef0ed601293c
burpdroid <script1337x@gmail.com>
2021-08-30 08:00:21 +0530

    xx

--- hacksec_cli/update.py
+++ hacksec_cli/update.py
@@ -18,7 +18,7 @@ def version_verify(current_version):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     result = loop.run_until_complete(check_for_update())
-    if result.decode() > current_version:
+    if float(result.decode()) > current_version:
         console.print("there is an upload available", style="bold red")
     with console.status("[bold green]updating...\n") as status:
         install_dir = os.path.join(
